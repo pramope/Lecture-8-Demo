c9d504f541d40820db7248ea11d8a12147310cca
Paula Ramos Perez <143439466+pramope@users.noreply.github.com>
2024-10-14 11:30:14 -0400

    changing to c

--- Hi.java
+++
@@ -1,5 +0,0 @@
-public class Hi {
-  public static void main() {
-    System.out.println("Hi everyone!");
-  }
-}
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+  printf("Welcome to Week 8 of CS193!\n");
+  return 0;
+}
